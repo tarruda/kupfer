782646436529d50641db6669cdf0e6c82d9d7308
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2010-01-13 02:24:00 +0100

    facebook: Working WIP

--- kupfer/plugin/facebook.py
+++ kupfer/plugin/facebook.py
@@ -5,7 +5,8 @@ from kupfer.objects import (TextLeaf, UrlLeaf, RunnableLeaf, FileLeaf,
 		AppLeafContentMixin )
 from kupfer import utils
 from kupfer.helplib import FilesystemWatchMixin
-from kupfer.obj.grouping import EMAIL_KEY, ContactLeaf, ToplevelGroupingSource
+from kupfer.obj.contacts import EMAIL_KEY, ContactLeaf
+from kupfer.obj.grouping import ToplevelGroupingSource
 
 __kupfer_name__ = _("Facebook")
 __kupfer_sources__ = ("ContactsSource", )
@@ -16,55 +17,56 @@ __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
 import facebook
 
-
-
-class Contact (ContactLeaf):
-	pass
-	
-
-class SendFileByMail(Action):
-	''' Createn new mail and attach selected file'''
-	def __init__(self):
-		Action.__init__(self, _('Send by Email'))
-
-	def activate(self, leaf):
-		filepath = leaf.object
-		utils.launch_commandline("claws-mail --attach '%s'" % filepath)
-
-	def get_icon_name(self):
-		return "mail-message-new"
-
-	def item_types(self):
-		yield FileLeaf
-
-	def get_description(self):
-		return _("Compose new email in ClawsMail and attach file")
-
-	def valid_for_item(self, item):
-		return os.path.isfile(item.object)
-
-
 API_KEY = "832b734b048412cd714707e9f25c7029"
 S_KEY = ""
 
+
+class AuthorizeInFacebook (RunnableLeaf):
+	def __init__(self):
+		RunnableLeaf.__init__(self, name=_("Authorize in Facebook"))
+	def run(self):
+		ContactsSource.authorize()
+
+class AuthorizationOK (RunnableLeaf):
+	def __init__(self):
+		RunnableLeaf.__init__(self, name=_("Authorization Finished"))
+	def run(self):
+		ContactsSource.authorize_finish()
+
+class Contact (ContactLeaf):
+	pass
+
 ToplevelGroupingSource = Source
 class ContactsSource (ToplevelGroupingSource):
-	def __init__(self, name=_("Claws Mail Address Book")):
+	shared_instance = None
+	def __init__(self, name=_("Facebook")):
 		ToplevelGroupingSource.__init__(self, _("Facebook")) #, "Contacts")
-		self._version = 1
+		self._version = 2
 		self._session_data = {}
 
 	def initialize(self):
 		ToplevelGroupingSource.initialize(self)
+		ContactsSource.shared_instance = self
+		self.connection = facebook.Facebook(API_KEY, S_KEY)
 		if self._session_data:
-			self.connection = facebook.Facebook(API_KEY, S_KEY)
 			self.connection.session_key = self._session_data["session_key"]
-		else:
-			self.connection = self._new_connection()
-			print "create token"
-			self.connection.auth.createToken()
-			self.connection.login()
+			self.connection.secret = self._session_data["secret"]
 	
+	@classmethod
+	def authorize(cls):
+		cls.shared_instance._authorize()
+	def _authorize(self):
+		self.connection = facebook.Facebook(API_KEY, S_KEY)
+		self.connection.auth.createToken()
+		self.connection.login()
+
+	@classmethod
+	def authorize_finish(cls):
+		cls.shared_instance._authorize_finish()
+	def _authorize_finish(self):
+		self._session_data = self.connection.auth.getSession()
+		self.mark_for_update()
+
 	def _new_connection(self):
 		connection = facebook.Facebook(API_KEY, S_KEY)
 		print "create token"
@@ -77,11 +79,17 @@ class ContactsSource (ToplevelGroupingSource):
 		#connection = facebook.Facebook(API_KEY, S_KEY)
 		#self.connection.session_key = self._session_data["session_key"]
 		#self.connection.auth.createToken()
+		if not self._session_data:
+			yield AuthorizeInFacebook()
+			yield AuthorizationOK()
+			return
 		try:
-			self._session_data = self.connection.auth.getSession()
-		finally:
-			self.connection = self._new_connection()
-			self._session_data = self.connection.auth.getSession()
+			self.connection.users.getLoggedInUser()
+		except facebook.FacebookError, exc:
+			self.output_error(type(exc).__name__, exc)
+			self._session_data = None
+			self.mark_for_update()
+			return
 		friend_ids = self.connection.friends.get()
 		print friend_ids
 		friends = self.connection.users.getInfo(friend_ids, fields=["pic_square", "profile_url", "name", "uid"])
